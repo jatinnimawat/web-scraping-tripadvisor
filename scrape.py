a038af4b26ae913355906ccc1e39d8f49929856f
jatinnimawat <nimawat.jatin03@gmail.com>
2022-04-09 22:06:48 -0400

    New changes on 9th April

--- scrape.py
+++ scrape.py
@@ -1,21 +1,4 @@
-# WEB SCRAPING PROJECT ON MOST IN-DEMAND JOB SKILLS
+from bs4 import BeautifulSoup as soup
 
-# Loading the web page and storing the html code in a variable
-from urllib.request import urlopen
-
-url = "http://olympus.realpython.org/profiles/dionysus"
-page = urlopen(url)
-page = page.read()
-page = page.decode("utf-8")
-print(page)
-
-print("Page is loaded in the code.\n")
-
-# Extracting data from the html webpage
-# Print the title of the page.
-import re
-
-string1 = re.search("<title.*?>.*?</title.*?>", page, re.IGNORECASE)
-string2 = string1.group()
-string2 = re.sub("<.*?>", "", string2)
-print(string2)
+url = "https://bigseventravel.com/50-most-visited-cities-in-the-world/"
+page = soup(url, "html.parser")
